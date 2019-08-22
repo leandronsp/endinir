76254869ca870d596835c8b5087c32e14e1e269f
Leandro Maríngolo <leandronsp@gmail.com>
2019-08-22 12:08:00 +0100

    update assembly tutorial

--- programming/assembly/osx-mojave/tutorial.asm
+++ programming/assembly/osx-mojave/tutorial.asm
@@ -1,35 +1,27 @@
-; Hello My Friend in assembly for mac
-; nasm -f macho hello.asm
-; ld -e _start -o hello hello.o
-;
-section     .text
- global _start                       ;must be declared for linker (ld)
+; nasm -f macho tutorial.asm
+; ld -e _start -o tutorial tutorial.o
+
+section .text
+global _start     ;must be declared for linker (ld)
 
 _syscall:
-     int     0x80            ;system call
-     ret
+  int 0x80        ;system call
+  ret
 
-_start:                         ;tell linker entry point
+_start:           ;tell linker entry point
+  push  dword len ;message length
+  push  dword msg ;message to write
+  push  dword 1   ;file descriptor (stdout)
+  mov   eax,0x4   ;system call number (sys_write)
 
-     push    dword len       ;message length
-     push    dword msg       ;message to write
-     push    dword 1         ;file descriptor (stdout)
-     mov     eax,0x4         ;system call number (sys_write)
-     call    _syscall        ;call kernel
+  call  _syscall  ;call kernel
 
-                             ;the alternate way to call kernel:
-                             ;push   eax
-                             ;call   7:0
+  add   esp,12    ;clean stack (3 arguments * 4)
+  push  dword 0   ;exit code
+  mov   eax,0x1   ;system call number (sys_exit)
 
-     add     esp,12          ;clean stack (3 arguments * 4)
+  call  _syscall  ;call kernel
 
-     push    dword 0         ;exit code
-     mov     eax,0x1         ;system call number (sys_exit)
-     call    _syscall        ;call kernel
-
-                             ;we do not return from sys_exit,
-                             ;there's no need to clean stack
 section .data
-
-msg     db      "hello world, engineer!",0xa     ;our dear string
-len     equ     $ - msg                 ;length of our dear string
+msg db  "hello world, engineer",0xa ;our dear string
+len equ $ - msg                     ;length of our dear string
--- programming/assembly/ubuntu-lts/tutorial.asm
+++ programming/assembly/ubuntu-lts/tutorial.asm
@@ -1,13 +1,21 @@
+; nasm -f elf64 tutorial.asm
+; ld -s -o tutorial tutorial.o
+
 section .text
 global _start ;must be declared for linker (ld)
-_start: ;tell linker entry point
-mov edx,len ;message length
-mov ecx,msg ;message to write
-mov ebx,1 ;file descriptor (stdout)
-mov eax,4 ;system call number (sys_write)
-int 0x80 ;call kernel
-mov eax,1 ;system call number (sys_exit)
-int 0x80 ;call kernel
+
+_start:       ;tell linker entry point
+  mov edx,len ;message length
+  mov ecx,msg ;message to write
+  mov ebx,1   ;file descriptor (stdout)
+  mov eax,4   ;system call number (sys_write)
+
+  int 0x80    ;call kernel
+
+  mov eax,1   ;system call number (sys_exit)
+
+  int 0x80    ;call kernel
+
 section .data
-msg db 'hello world, engineer',0xa ;our dear string
-len equ $ - msg ;length of our dear string
+msg db "hello world, engineer",0xa ;our dear string
+len equ $ - msg                    ;length of our dear string
